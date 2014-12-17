7834eaad9bddfbeef5969d886c263925617ef2ae
emSebastian Sjödin/em <ss1337@users.noreply.github.com>
2014-12-17 14:23:11 +0100

    Verbosity

--- BeverDrive/Modules/AModule.cs
+++ BeverDrive/Modules/AModule.cs
@@ -20,9 +20,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using BeverDrive.Core;
-using System.Reflection;
 using BeverDrive.Gui.Controls;
 
 namespace BeverDrive.Modules
