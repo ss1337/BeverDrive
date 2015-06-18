c832cebc36cc7ed4a61bbcbaf5d3237119e53b3c
emSebastian Sjödin/em <ss1337@users.noreply.github.com>
2015-06-18 18:49:49 +0200

    Capitalization...

--- BeverDrive.Ibus/IbusContext.cs
+++ BeverDrive.Ibus/IbusContext.cs
@@ -42,7 +42,7 @@ namespace BeverDrive.Ibus
 	public delegate void InitEventHandler(object sender, EventArgs e);
 	public delegate void ValidMessageEventHandler(object sender, ValidMessageRecievedEventArgs e);
 
-	public class IbusContext : IIBusContext
+	public class IbusContext : IIbusContext
 	{
 		public Mode CurrentMode { get; private set; }
 
